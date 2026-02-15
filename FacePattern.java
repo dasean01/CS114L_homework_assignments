e49f37f3e3bd8aba2f7707722e9ddc06ccada23c
dasean01 <dasean193@gmail.com>
2026-02-15 17:34:36 -0500

    Add files via upload

---
+++ FacePattern.java
@@ -0,0 +1,13 @@
+public class FacePattern.java{
+    public static void main(String[] args) {
+        System.out.printf( "+'''''+");
+        System.out.printf("[| o o |]");
+        System.out.printf(" |  ^  |");
+        System.out.printf(" | '_' |");
+        System.out.ptintf(" +_____+");
+
+    }
+}
+
+void main() {
+}
