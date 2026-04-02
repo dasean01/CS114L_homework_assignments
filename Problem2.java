b32c5be8c74942c9a8ca278daecd80b512aea425
dasean01 <dasean193@gmail.com>
2026-04-02 18:16:35 -0400

    Add files via upload

---
+++ Problem2.java
@@ -0,0 +1,21 @@
+public class Problem2 {
+    public static int singleNumber(int[] nums) {
+        int result = 0;
+
+        for (int num : nums) {
+            result ^= num;
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = {2, 2, 1};
+        int[] nums2 = {4, 1, 2, 1, 2};
+        int[] nums3 = {1};
+
+        System.out.println("Output 1: " + singleNumber(nums1));
+        System.out.println("Output 2: " + singleNumber(nums2));
+        System.out.println("Output 3: " + singleNumber(nums3));
+    }
+}
